2c68b6a01976e05ac157cc5b256b6e3f49a14215
Lingxi Li <lilingxi01@gmail.com>
2019-10-19 04:28:01 -0400

    Build the base of the Exploration Fragment.

--- app/src/main/java/cn/tacitech/umasslife/Fragments/ExplorationFragment.java
+++ app/src/main/java/cn/tacitech/umasslife/Fragments/ExplorationFragment.java
@@ -1,7 +1,73 @@
 package cn.tacitech.umasslife.Fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
-public class ExplorationFragment extends Fragment {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import cn.tacitech.umasslife.R;
+
+public class ExplorationFragment extends Fragment implements OnMapReadyCallback {
+
+
+    /**
+     * Exploration Fragment
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_exploration, container, false);
+
+        MapView mMap = view.findViewById(R.id.mapView);
+        mMap.onCreate(savedInstanceState);
+        mMap.onResume();
+        try {
+            MapsInitializer.initialize(getActivity());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        int errorCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity());
+        if (ConnectionResult.SUCCESS != errorCode) {
+            GooglePlayServicesUtil.getErrorDialog(errorCode, getActivity(), 0).show();
+        } else {
+            mMap.getMapAsync(this);
+        }
+        return view;
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        double lat = 40.73;
+        double lng = -73.99;
+        LatLng appointLoc = new LatLng(lat, lng);
+
+        // 移动地图到指定经度的位置
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(appointLoc));
+
+        //添加标记到指定经纬度
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Marker")
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher)));
+
+
+    }
 }
