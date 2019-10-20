d59748d14d7b798f2d1897a80203a0f6259b9bd3
l497996153 <38828371+l497996153@users.noreply.github.com>
2019-10-20 01:35:29 -0400

    update Dining Detail Activity

--- app/src/main/java/cn/tacitech/umasslife/DiningDetailActivity.java
+++ app/src/main/java/cn/tacitech/umasslife/DiningDetailActivity.java
@@ -3,12 +3,25 @@ package cn.tacitech.umasslife;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DiningDetailActivity extends AppCompatActivity {
+    private String nameStr = "abc";
+    private String descriptionStr = "def.";
+    private TextView name;
+    private TextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dining_detail);
+        name = (TextView) findViewById(R.id.course);
+        description = (TextView) findViewById(R.id.contentTitle);
+        initialize(name, description);
     }
+    protected void initialize(TextView name, TextView description){
+        name.setText(nameStr);
+        description.setText(descriptionStr);
+    }
+
 }
