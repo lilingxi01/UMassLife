fbfe203865de779614d6c9eb2f690c60ca5e0c9c
l497996153 <38828371+l497996153@users.noreply.github.com>
2019-10-20 01:14:37 -0400

    create dininglist activity

---
+++ app/src/main/java/cn/tacitech/umasslife/DiningListActivity.java
@@ -0,0 +1,14 @@
+package cn.tacitech.umasslife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiningListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dining_list);
+    }
+}
