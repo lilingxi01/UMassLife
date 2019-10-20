1f6a7edf21e12264bba48b0d17d47424528095e5
l497996153 <38828371+l497996153@users.noreply.github.com>
2019-10-19 22:06:25 -0400

    create AlertDetailsActivity

---
+++ app/src/main/java/cn/tacitech/umasslife/AlertDetailsActivity.java
@@ -0,0 +1,14 @@
+package cn.tacitech.umasslife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlertDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert_details);
+    }
+}
