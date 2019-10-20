a6970d38336a79177a8c9b4cf6352baa435e2822
l497996153 <38828371+l497996153@users.noreply.github.com>
2019-10-20 01:25:06 -0400

    create dining detail activity

---
+++ app/src/main/java/cn/tacitech/umasslife/DiningDetailActivity.java
@@ -0,0 +1,14 @@
+package cn.tacitech.umasslife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DiningDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dining_detail);
+    }
+}
