70a5b1352bb3c315b9d03f660e6c3e705b071e27
Lingxi Li <lilingxi01@gmail.com>
2019-10-20 05:04:05 -0400

    Finish loginPage!

--- app/src/main/java/cn/tacitech/umasslife/LoginActivity.java
+++ app/src/main/java/cn/tacitech/umasslife/LoginActivity.java
@@ -1,10 +1,13 @@
 package cn.tacitech.umasslife;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -13,6 +16,9 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.fivehundredpx.android.blur.BlurringView;
+
+import org.w3c.dom.Text;
+
 import cn.tacitech.umasslife.Modules.UiModule;
 
 public class LoginActivity extends AppCompatActivity {
@@ -22,9 +28,12 @@ public class LoginActivity extends AppCompatActivity {
 
     private RelativeLayout login_netid_field;
     private RelativeLayout login_key_field;
+    private RelativeLayout login_test_info;
 
     private EditText login_netid_input;
     private EditText login_key_input;
+
+    private CardView login_btn;
 
     private BlurringView mBlurringView;
 
@@ -42,7 +51,6 @@ public class LoginActivity extends AppCompatActivity {
 
         LinearLayout login_basic_layout = findViewById(R.id.login_basic_layout);
         mBlurringView = findViewById(R.id.blurring_view);
-        mBlurringView.setVisibility(View.GONE);
         mBlurringView.setBlurredView(login_basic_layout);
 
         // Login Netid Text
@@ -60,6 +68,7 @@ public class LoginActivity extends AppCompatActivity {
         // Fields
         login_netid_field = findViewById(R.id.login_netid_field);
         login_key_field = findViewById(R.id.login_key_field);
+        login_test_info = findViewById(R.id.login_test_info);
 
         // Inputs
         login_netid_input = findViewById(R.id.login_netid_input);
@@ -108,6 +117,47 @@ public class LoginActivity extends AppCompatActivity {
                 closeAllField();
             }
         });
+        RelativeLayout login_test_info_bg = findViewById(R.id.login_key_field_bg);
+        login_test_info_bg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                closeAllField();
+            }
+        });
+
+        // Login Btn
+        login_btn = findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username_new = login_netid_input.getText().toString();
+                String password_new = login_key_input.getText().toString();
+                loginRequest(username_new, password_new);
+            }
+        });
+
+        // Testing Information
+        TextView login_test_info_title = findViewById(R.id.login_test_info_title);
+        login_test_info_title.setTypeface(UiModule.getTypeface(this, UiModule.DIN)); // 设置字体
+        TextView login_test_info_content = findViewById(R.id.login_test_info_content);
+        login_test_info_content.setTypeface(UiModule.getTypeface(this, UiModule.DIN)); // 设置字体
+        login_test_info_content.setText("Testing NetID: dzhong\n" +
+                "Testing Password: dzhong\n\n---\n\n" +
+                "Time is so limited that we cannot finish everything for this app.\n" +
+                "If you all like it and want it, we will bring it to the real life for sure!\n" +
+                "We are now trying to gain the API of campus system to make further!\n" +
+                "Again, thanks for your testing and support~"
+        );
+
+
+    }
+
+    public void loginRequest(String netid, String psw){
+        if(netid.equals(test_netid) && psw.equals(test_password)){
+            finish();
+        } else {
+            login_btn.setCardBackgroundColor(getResources().getColor(R.color.used_red));
+        }
     }
 
     public void closeAllField(){
@@ -115,6 +165,7 @@ public class LoginActivity extends AppCompatActivity {
         closeSoftKeybord(login_netid_input);
         closeSoftKeybord(login_key_input);
         mBlurringView.setVisibility(View.GONE);
+        login_test_info.setVisibility(View.GONE);
         login_netid_field.setVisibility(View.GONE);
         login_key_field.setVisibility(View.GONE);
     }
@@ -152,5 +203,13 @@ public class LoginActivity extends AppCompatActivity {
     public void closeSoftKeybord(EditText mEditText) {
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return true;
+        }
+        return super.onKeyDown(keyCode,event);
     }
 }
